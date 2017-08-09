ca4dfa78d651d163c59023c010bd74d9093f5ccf
josera21 <jochix21@gmail.com>
2017-08-09 12:03:47 -0400

    Challenge complete.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
+	$('#quoteCard').hide();
+
 	$('#quoteAJAX').click(function() {
+		$('#quoteCard').fadeIn(1000);
+
 		$.ajax({
 		  url: "https://api.forismatic.com/api/1.0/",
 		  jsonp: "jsonp",
@@ -17,6 +21,11 @@ $(document).ready(function() {
 	function update(response) {
 	  $('#log').prepend('<pre>' + $('#response').html() + '</pre>');
 	  $('#quoteText').html(JSON.stringify(response.quoteText));
+	  $('#quoteAuthor').html(JSON.stringify(response.quoteAuthor));
+	  
+	  // Para twettear
+	  var urlTwitter = 'https://twitter.com/intent/tweet?text=';
+	  $('#tweet').attr("href",urlTwitter + $('#quoteText').html() + $('#quoteAuthor').html());
 	}
 
 	function handleErr(jqxhr, textStatus, err) {
